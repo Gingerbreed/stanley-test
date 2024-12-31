3eaa9b5bdfd04820756132d143187ebdde031728
Morgan Baker <morgan@mbaker.org>
2024-12-31 00:19:30 -0500

    add results component

---
+++ src/Results.tsx
@@ -0,0 +1,13 @@
+export default function Results({ rs }: { rs: Result }) {
+  return (
+    <div>
+      <h3> {rs.productName} </h3>
+      <div>
+        <p>Product Id: {rs.productId}</p>
+        <p>Metro Area: {rs.metroArea}</p>
+        <p>Full Name: {rs.fullName}</p>
+        <p>Project Group Id: {rs.projectGroupId}</p>
+      </div>
+    </div>
+  );
+}
