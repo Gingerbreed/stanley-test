2e3d4679ada4f143a78d74406190daaeea937c94
Morgan Baker <morgan@mbaker.org>
2024-12-31 00:20:24 -0500

    adding div for showing resultslist component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Interview Title
-        </p>
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="searchInput">Search:</label>
-        <input id="searchInput" type="text" placeholder='Please Enter A Search Term'/>
-        <button type="submit">Submit</button>
-      </div>
-      
-    </form>
+        <p>Interview Title</p>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="searchInput">Search:</label>
+            <input
+              id="searchInput"
+              type="text"
+              placeholder="Please Enter A Search Term"
+            />
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </header>
+      <div className="Results"></div>
     </div>
   );
 }
 
 function handleSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
-  event.preventDefault()
-  const form = event.currentTarget
+  event.preventDefault();
+  const form = event.currentTarget;
   const formElements = form.elements as typeof form.elements & {
-    searchInput: {value: string}
-  }
-  console.log("safe, plus:" + formElements.searchInput?.value )
- //onSubmitUsername(formElements.usernameInput.value)
+    searchInput: { value: string };
+  };
+  console.log("safe, plus:" + formElements.searchInput?.value);
+  //onSubmitUsername(formElements.usernameInput.value)
 }
 
 export default App;
