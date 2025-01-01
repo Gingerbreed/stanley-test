0f86fa60ee318284b6d55bedab1b88b55b10ffa0
Morgan Baker <morgan@mbaker.org>
2025-01-01 15:03:49 -0500

    fixing up nulls

--- src/Results.tsx
+++ src/Results.tsx
@@ -3,12 +3,26 @@ import { Result } from "./types/Result";
 export default function Results({ rs }: { rs: Result }) {
   return (
     <div className={"indigo"}>
-      <h3> {rs.product?.productName} </h3>
+      <h3>
+        {" "}
+        {rs.product?.productName
+          ? rs.product.productName
+          : "No Product Name"}{" "}
+      </h3>
       <div>
-        <p>Product Id: {rs.product?.productId}</p>
+        <p>
+          Product Id: {rs.product?.productId ? rs.product.productId : "No Id"}
+        </p>
         <p>Metro Area: {rs.metro?.metroAreaTitle}</p>
-        <p>Full Name: {rs.project?.fullName}</p>
-        <p>Project Group Id: {rs.project?.projectGroupId}</p>
+        <p>
+          Full Name: {rs.project?.fullName ? rs.project.fullName : "No Project"}
+        </p>
+        <p>
+          Project Group Id:{" "}
+          {rs.project?.projectGroupId
+            ? rs.project.projectGroupId
+            : "No Project"}
+        </p>
       </div>
     </div>
   );
